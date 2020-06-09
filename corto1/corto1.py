baad1bdff60f8a470fcc2311dc3b0b1277df74d6
usac201114651 <2096434980412@ingenieria.usac.edu.gt>
2020-06-09 12:40:21 -0600

    corto1

---
+++ corto1/corto1.py
@@ -0,0 +1,23 @@
+
+print ("corto1")
+
+NC = 651 #numero de carne
+n = NC
+
+def accion (n): #accion que ejecuta la secuencia
+a=0
+   while a != 1:
+    if n%2 == 0:
+        a = n/2
+        return (a)
+    else:
+        a = n * 3 +1
+        return (a)
+
+def imprimir (a)
+for i in range (1, a)
+
+
+
+
+
